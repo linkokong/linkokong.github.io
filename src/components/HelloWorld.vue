19dd61d4c2b8fc1169d42c9a95061dc7bab31473
konglinjian <lingjian.kong@coinbest.com>
2023-10-31 18:32:35 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,78 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,computed } from 'vue'
+import { NDataTable,NTabs,NTabPane,NInput,NSpace,darkTheme,NConfigProvider} from 'naive-ui'
+import {middle_1 } from '../dictionary/middle_1'
+import {middle_2 } from '../dictionary/middle_2'
+import {primary_1 } from '../dictionary/primary_1'
+import {primary_2 } from '../dictionary/primary_2'
+import {n2 } from '../dictionary/n2'
 
-defineProps({
-  msg: String
+const words={middle_1,middle_2,primary_1,primary_2,n2}
+
+const columns = ref([
+  {title: "カタカナ", key: "kana"},
+  {title: "漢字", key: "kanji"},
+  {title: "中文", key: "chinese"},
+]);
+const chapValue=ref('primary_2')
+
+
+const wordlist=computed({
+  get:()=>{
+    if(filterKeyword.value){
+      return words[chapValue.value]?.filter(item=>{
+        let letter_t=item.kana+item.kanji+item.chinese
+        if(letter_t.indexOf(filterKeyword.value)>-1){
+          return item
+        }
+      })||[]
+    }
+    return words[chapValue.value]
+  },
+  set:(val)=>{}
 })
 
-const count = ref(0)
+let filterKeyword=ref(null)
+
+let theme =ref(darkTheme)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <n-space vertical>
+      <n-tabs type="segment" v-model:value="chapValue">
+        <n-tab-pane name="primary_1" tab="初级上">
+        </n-tab-pane>
+        <n-tab-pane name="primary_2" tab="初级下">
+        </n-tab-pane>
+        <n-tab-pane name="middle_1" tab="中级上">
+        </n-tab-pane>
+        <n-tab-pane name="middle_2" tab="中级下">
+        </n-tab-pane>
+        <n-tab-pane name="n2" tab="N2">
+        </n-tab-pane>
+      </n-tabs>
+    </n-space>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+
+  <n-space vertical>
+      <n-input v-model:value="filterKeyword" type="text" tex align='left' placeholder=""/>
+  </n-space>
+
+
+  <n-config-provider :theme="theme">
+  <n-space style='padding-top:5px'>
+      <n-data-table
+        :columns="columns"
+        :data="wordlist"
+        virtual-scroll
+        :max-height="1000"
+        size='small'
+        striped
+        :single-line='false'
+      />
+  </n-space>
+  </n-config-provider>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
